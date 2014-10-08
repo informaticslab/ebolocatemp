c4f50ec142bf12d3ce49c06b0ba3d82ccb103f10
pwhitebe <pwhitebe@gmail.com>
2014-10-08 10:28:08 -0400

    Removed all the references to authentication/authorization for now.

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
  var express = require('express')
-     , passport = require('passport')
-  , flash = require('connect-flash')
+  //    , passport = require('passport')
+   , flash = require('connect-flash')
   , util = require('util')
-  , LocalStrategy = require('passport-local').Strategy
-, BearerStrategy = require('passport-http-bearer').Strategy;
+//   , LocalStrategy = require('passport-local').Strategy
+// , BearerStrategy = require('passport-http-bearer').Strategy;
     record = require('./routes/record');
 users = require('./routes/users');
 var mongo = require('mongodb');
@@ -14,8 +14,8 @@ var monk = require('monk');
 //    BSON = mongo.BSONPure;
  
 var app = express();
-var db = monk('localhost:27017/userdb');
-var userdb = db.get('users');
+// var db = monk('localhost:27017/userdb');
+// var userdb = db.get('users');
 //var server = new Server('localhost', 27017, {auto_reconnect: true});
 //var userdb = new Db('userdb', server);
 //var users = [
@@ -23,122 +23,122 @@ var userdb = db.get('users');
 //  , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com', token: '987654321' }
 //];
 
-function findById(id, fn) {
-  //var idx = id - 1;
-  //if (userdb[idx]) {
-  //  fn(null, users[idx]);
-  //} else {
-   // fn(new Error('User ' + id + ' does not exist'));
-  //}
-   userdb.findById(id, function(err,user){
-                if(err) done(err);
-                done(null,user);
-            });
-}
+// function findById(id, fn) {
+//   //var idx = id - 1;
+//   //if (userdb[idx]) {
+//   //  fn(null, users[idx]);
+//   //} else {
+//    // fn(new Error('User ' + id + ' does not exist'));
+//   //}
+//    userdb.findById(id, function(err,user){
+//                 if(err) done(err);
+//                 done(null,user);
+//             });
+// }
 
-function findByUsername(username, fn) {
-//  for (var i = 0, len = users.length; i < len; i++) {
-//    var user = users[i];
-//    if (user.username === username) {
-//      return fn(null, user);
-//    }
-//  }
-//  return fn(null, null);
-userdb.findOne({ username : username},function(err,user, done){
-   console.log('found user: ' + username + ", the user id is " + user.id);
+// function findByUsername(username, fn) {
+// //  for (var i = 0, len = users.length; i < len; i++) {
+// //    var user = users[i];
+// //    if (user.username === username) {
+// //      return fn(null, user);
+// //    }
+// //  }
+// //  return fn(null, null);
+// userdb.findOne({ username : username},function(err,user, done){
+//    console.log('found user: ' + username + ", the user id is " + user.id);
        
-//        if(err) { return done(err); }
-//        if(!user){
-//            console.log('apparently there was no user' + user);
-//            return done({ message: 'Incorrect username.' });
-//        }
-        return user;
-});
-};
+// //        if(err) { return done(err); }
+// //        if(!user){
+// //            console.log('apparently there was no user' + user);
+// //            return done({ message: 'Incorrect username.' });
+// //        }
+//         return user;
+// });
+// };
 
-function findByToken(token, fn) {
-//  for (var i = 0, len = users.length; i < len; i++) {
-//    var user = users[i];
-//    if (user.token === token) {
-//      return fn(null, user);
-//    }
-//  }
-//  return fn(null, null);
-userdb.findOne({ token : token},function(err,user, done){
-    console.log("token was " + token)
-        if(err) { return done(err); }
-        if(!user){
-            return done(null, false, { message: 'Nonexistant token.' });
-        }
-        return user;
-})
-};
+// function findByToken(token, fn) {
+// //  for (var i = 0, len = users.length; i < len; i++) {
+// //    var user = users[i];
+// //    if (user.token === token) {
+// //      return fn(null, user);
+// //    }
+// //  }
+// //  return fn(null, null);
+// userdb.findOne({ token : token},function(err,user, done){
+//     console.log("token was " + token)
+//         if(err) { return done(err); }
+//         if(!user){
+//             return done(null, false, { message: 'Nonexistant token.' });
+//         }
+//         return user;
+// })
+// };
 
 // Use the BearerStrategy within Passport.
 //   Strategies in Passport require a `validate` function, which accept
 //   credentials (in this case, a token), and invoke a callback with a user
 //   object.
-passport.use(new BearerStrategy({
-  },
-  function(token, done) {
-    // asynchronous validation, for effect...
-    process.nextTick(function () {
+// passport.use(new BearerStrategy({
+//   },
+//   function(token, done) {
+//     // asynchronous validation, for effect...
+//     process.nextTick(function () {
       
-      // Find the user by token.  If there is no user with the given token, set
-      // the user to `false` to indicate failure.  Otherwise, return the
-      // authenticated `user`.  Note that in a production-ready application, one
-      // would want to validate the token for authenticity.
-      findByToken(token, function(err, user) {
-        if (err) { return done(err); }
-        if (!user) { return done(null, false); }
-        return done(null, user);
-      })
-    });
-  }
-));
+//       // Find the user by token.  If there is no user with the given token, set
+//       // the user to `false` to indicate failure.  Otherwise, return the
+//       // authenticated `user`.  Note that in a production-ready application, one
+//       // would want to validate the token for authenticity.
+//       findByToken(token, function(err, user) {
+//         if (err) { return done(err); }
+//         if (!user) { return done(null, false); }
+//         return done(null, user);
+//       })
+//     });
+//   }
+// ));
 
 
-// Passport session setup.
-//   To support persistent login sessions, Passport needs to be able to
-//   serialize users into and deserialize users out of the session.  Typically,
-//   this will be as simple as storing the user ID when serializing, and finding
-//   the user by ID when deserializing.
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
+// // Passport session setup.
+// //   To support persistent login sessions, Passport needs to be able to
+// //   serialize users into and deserialize users out of the session.  Typically,
+// //   this will be as simple as storing the user ID when serializing, and finding
+// //   the user by ID when deserializing.
+// passport.serializeUser(function(user, done) {
+//   done(null, user.id);
+// });
 
-passport.deserializeUser(function(id, done) {
-  findById(id, function (err, user) {
-    done(err, user);
-  });
-});
+// passport.deserializeUser(function(id, done) {
+//   findById(id, function (err, user) {
+//     done(err, user);
+//   });
+// });
 
 
-// Use the LocalStrategy within Passport.
-//   Strategies in passport require a `verify` function, which accept
-//   credentials (in this case, a username and password), and invoke a callback
-//   with a user object.  In the real world, this would query a database;
-//   however, in this example we are using a baked-in set of users.
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
+// // Use the LocalStrategy within Passport.
+// //   Strategies in passport require a `verify` function, which accept
+// //   credentials (in this case, a username and password), and invoke a callback
+// //   with a user object.  In the real world, this would query a database;
+// //   however, in this example we are using a baked-in set of users.
+// passport.use(new LocalStrategy(
+//   function(username, password, done) {
+//     // asynchronous verification, for effect...
+//     process.nextTick(function () {
       
-      // Find the user by username.  If there is no user with the given
-      // username, or the password is not correct, set the user to `false` to
-      // indicate failure and set a flash message.  Otherwise, return the
-      // authenticated `user`.
-      findByUsername(username, function(err, user) {
-        if (err) { return done(err); }
-        if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
-        if (user.password !== password) { return done(null, false, { message: 'Invalid password' }); }
-        return done(null, user);
+//       // Find the user by username.  If there is no user with the given
+//       // username, or the password is not correct, set the user to `false` to
+//       // indicate failure and set a flash message.  Otherwise, return the
+//       // authenticated `user`.
+//       findByUsername(username, function(err, user) {
+//         if (err) { return done(err); }
+//         if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+//         if (user.password !== password) { return done(null, false, { message: 'Invalid password' }); }
+//         return done(null, user);
       
       
-      })
-    });
-  }
-));
+//       })
+//     });
+//   }
+// ));
 
  
 app.configure(function () {
@@ -148,13 +148,13 @@ app.configure(function () {
     app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
     app.use(express.bodyParser());
     app.use(express.cookieParser());
-     app.use(express.methodOverride());
-  app.use(express.session({ secret: 'keyboard cat' }));
-  app.use(flash());
+    app.use(express.methodOverride());
+   app.use(express.session({ secret: 'keyboard cat' }));
+   app.use(flash());
   // Initialize Passport!  Also use passport.session() middleware, to support
   // persistent login sessions (recommended).
-  app.use(passport.initialize());
-  app.use(passport.session());
+  // app.use(passport.initialize());
+  // app.use(passport.session());
   app.use(app.router);
   app.use(express.static(__dirname + '/../../public'));
 });
@@ -167,16 +167,17 @@ app.post('/ebolocatemp/api/record', record.addRecord);
 //app.delete('/record/:id', record.deleteArticle);
 
 app.get('/ebolocatemp/', function(req, res){
-  res.render('index', { user: req.user });
+  //res.render('index', { user: req.user });
+  res.render('index');
 });
 
-app.get('/ebolocatemp/account', ensureAuthenticated, function(req, res){
-  res.render('account', { user: req.user });
-});
+// app.get('/ebolocatemp/account', ensureAuthenticated, function(req, res){
+//   res.render('account', { user: req.user });
+// });
 
-app.get('/ebolocatemp/login', function(req, res){
-  res.render('login', { user: req.user, message: req.flash('error') });
-});
+// app.get('/ebolocatemp/login', function(req, res){
+//   res.render('login', { user: req.user, message: req.flash('error') });
+// });
 
 // POST /login
 //   Use passport.authenticate() as route middleware to authenticate the
@@ -185,18 +186,18 @@ app.get('/ebolocatemp/login', function(req, res){
 //   which, in this example, will redirect the user to the home page.
 //
 //   curl -v -d "username=bob&password=secret" http://127.0.0.1:3000/login
-app.post('/ebolocatemp/login', 
-  passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
-  function(req, res) {
-    res.redirect('/');
-  });
+// app.post('/ebolocatemp/login', 
+//   passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
+//   function(req, res) {
+//     res.redirect('/');
+//   });
 
-app.get('/ebolocatemp/foo',
-  // Authenticate using HTTP Bearer credentials, with session support disabled.
-  passport.authenticate('bearer', { session: false }),
-  function(req, res){
-    res.json({ username: req.user.username, email: req.user.email });
-  });
+// app.get('/ebolocatemp/foo',
+//   // Authenticate using HTTP Bearer credentials, with session support disabled.
+//   passport.authenticate('bearer', { session: false }),
+//   function(req, res){
+//     res.json({ username: req.user.username, email: req.user.email });
+//   });
   
 // POST /login
 //   This is an alternative implementation that uses a custom callback to
@@ -217,10 +218,10 @@ app.post('/login', function(req, res, next) {
 });
 */
 
-app.get('/ebolocatemp/logout', function(req, res){
-  req.logout();
-  res.redirect('/ebolocatemp/');
-});
+// app.get('/ebolocatemp/logout', function(req, res){
+//   req.logout();
+//   res.redirect('/ebolocatemp/');
+// });
 
 
 app.listen(8081);
@@ -231,7 +232,7 @@ console.log('Listening on port 8081...');
 //   the request is authenticated (typically via a persistent login session),
 //   the request will proceed.  Otherwise, the user will be redirected to the
 //   login page.
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/ebolocatemp/login');
-};
+// function ensureAuthenticated(req, res, next) {
+//   if (req.isAuthenticated()) { return next(); }
+//   res.redirect('/ebolocatemp/login');
+// };
