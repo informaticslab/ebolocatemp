8b62877049b3d8d4fc8c0b33b9a00552003aa660
Ben Elder <belder@deloitte.com>
2014-10-08 12:30:42 -0400

    cleaning up unused code

--- server.js
+++ server.js
@@ -1,67 +1,53 @@
- var express = require('express')
-  //    , passport = require('passport')
-   , flash = require('connect-flash')
-  , util = require('util')
-//   , LocalStrategy = require('passport-local').Strategy
-// , BearerStrategy = require('passport-http-bearer').Strategy;
+var express = require('express'), 
+  flash = require('connect-flash'), 
+  util = require('util'),
+  record = require('./routes/record');
 
-    record = require('./routes/record');
-//users = require('./routes/users');
-var mongo = require('mongodb');
-var monk = require('monk');
- 
-//var Server = mongo.Server,
-//    Db = mongo.Db,
-//    BSON = mongo.BSONPure;
- 
 var app = express();
  
 app.configure(function () {
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'ejs');
-    app.engine('ejs', require('ejs-locals'));
-    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
-    app.use(express.bodyParser());
-    app.use(express.cookieParser());
-    app.use(express.methodOverride());
-   app.use(express.session({ secret: 'keyboard cat' }));
-   app.use(flash());
-  // Initialize Passport!  Also use passport.session() middleware, to support
-  // persistent login sessions (recommended).
-  // app.use(passport.initialize());
-  // app.use(passport.session());
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.engine('ejs', require('ejs-locals'));
+  app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.methodOverride());
+  app.use(express.session({ secret: 'keyboard cat' }));
+  app.use(flash());
   app.use(app.router);
   app.use(express.static(__dirname + '/../../public'));
 });
  
-app.get('/ebolocatemp/api/record',
-        record.findAll);
+app.get('/ebolocatemp/api/record', record.findAll);
 app.get('/ebolocatemp/api/record/:id', record.findById);
 app.post('/ebolocatemp/api/record', record.addRecord);
-//app.put('/record/:id', record.updateArticle);
-//app.delete('/record/:id', record.deleteArticle);
 
 app.get('/ebolocatemp/', function(req, res){
-  //res.render('index', { user: req.user });
   res.render('index');
 });
 
 app.get('/record', function(req, res){
   res.render('record', { 
+    title: 'Temperature Collection Form',
     cdcId: req.query.cdcId
   });
 });
 
 app.get('/confirmation', function(req, res){
-  res.render('confirmation');
+  res.render('confirmation', {
+    title: 'Temperature Collection Confirmation'
+  });
 });
 
 app.get('/error', function(req, res){
-  res.render('error');
+  res.render('error', {
+    title: 'Temperature Collection Error'
+  });
 });
 
 app.post('/record', function(req, res){
-  console.log('record posted...');
+
   var bod = req.body;
   
   // validate posted data
@@ -74,7 +60,11 @@ app.post('/record', function(req, res){
   var toSave = {
     cdcId: bod.cdcId,
     temp: bod.temp,
+    loc: bod.loc,
+    timestamp: new Date().getTime()
   };
+
+  console.log('saving' + JSON.stringify(toSave));
 
   res.redirect('/confirmation');
 });
@@ -82,13 +72,3 @@ app.post('/record', function(req, res){
 
 app.listen(8092);
 console.log('Listening on port 8092...');
-
-// Simple route middleware to ensure user is authenticated.
-//   Use this route middleware on any resource that needs to be protected.  If
-//   the request is authenticated (typically via a persistent login session),
-//   the request will proceed.  Otherwise, the user will be redirected to the
-//   login page.
-// function ensureAuthenticated(req, res, next) {
-//   if (req.isAuthenticated()) { return next(); }
-//   res.redirect('/ebolocatemp/login');
-// };
